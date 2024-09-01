bbbdf93d23fd49bc1ed35cdc77d9af8ff04a7a35
devREMY1 <dev.maksyuta@gmail.com>
2024-09-01 15:19:09 +0300

    004/1

---
+++ 004/004-1.js
@@ -0,0 +1,15 @@
+let bool = true;
+let countF = 1;
+let countS = 1;
+
+while (bool) {
+	console.log(countF + ' x ' + countS + ' = ' + countF * countS);
+	countS++;
+	if (countS === 11) {
+		countF++;
+		countS = 1;
+	}
+	if (countF === 11) {
+		break;
+	}
+}
