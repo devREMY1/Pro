4ea52e80b5b183db1dd31d4b76dd6de2cb6662fc
devREMY1 <dev.maksyuta@gmail.com>
2024-09-09 22:34:29 +0300

    006/6

---
+++ 006/006-6.js
@@ -0,0 +1,12 @@
+let inputValue = 123;
+
+console.log('Type of inputValue =', typeof inputValue);
+
+let stringValue = String(inputValue);
+console.log('inputValue as string =', stringValue);
+
+let numberValue = Number(inputValue);
+console.log('inputValue as number =', numberValue);
+
+let booleanValue = Boolean(inputValue);
+console.log('inputValue as boolean =', booleanValue);
