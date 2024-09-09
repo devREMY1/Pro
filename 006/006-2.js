bb41d6a7cd658d07786753610f2ab5fe61a8c6c0
devREMY1 <dev.maksyuta@gmail.com>
2024-09-09 21:55:49 +0300

    006/2

---
+++ 006/006-2.js
@@ -0,0 +1,9 @@
+let numString = '42';
+
+let numInt = parseInt(numString);
+
+let numFloat = parseFloat(numString);
+
+console.log('numString =', numString);
+console.log('numInt =', numInt);
+console.log('numFloat =', numFloat);
